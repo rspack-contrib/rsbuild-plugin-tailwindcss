e9653f53f0e7fbdb22fea2cef7a7a4108dcaa061
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-02 14:55:49 +0800

    fix: getRandomPort

--- test/cjs/index.test.ts
+++ test/cjs/index.test.ts
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'node:url';
 import { expect, test } from '@playwright/test';
 import { createRsbuild } from '@rsbuild/core';
 import { pluginTailwindCSS } from '../../src';
+import { getRandomPort } from '../helper';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -75,7 +76,7 @@ test('should build without tailwind.config.js', async ({ page }) => {
     cwd: __dirname,
     rsbuildConfig: {
       server: {
-        port: Math.ceil(Math.random() * 30000) + 15000,
+        port: getRandomPort(),
       },
       plugins: [pluginTailwindCSS()],
     },
